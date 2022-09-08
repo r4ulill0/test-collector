043f47098b0d47030ee21f81e4fa52e5ef063480
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2022-09-08 14:26:52 +0200

    Versión funcional de escaneado de htmls de revisión producidos en plataformas moodle

--- src/collector.py
+++ src/collector.py
@@ -19,6 +19,7 @@ class Collector(HTMLParser):
     fase_respuesta_correcta = False
     respuesta_correcta_actual = ''
     leyendo_linea = False
+    prefijo_detectado = False
     p_count = 0
     span_count = 0
 
@@ -40,19 +41,48 @@ class Collector(HTMLParser):
                 # detectamos el div de 'feedback'
                 elif (att[0] == 'class' and re.match(r'.*outcome.*', att[1])):
                     self.fase_respuesta = False
+
                 # Detección de respuestas
                 elif (att[0] == 'data-region' and att[1] == "answer-label"):
                     self.respuesta_actual += 1
-                    self.fase_enunciado = False
                     self.fase_respuesta = True
                 elif (att[0] == 'class' and att[1] == 'rightanswer'):
                     self.fase_respuesta_correcta = True
+                    self.leyendo_linea = False  # Evitamos leer la muletilla
+
         elif (tag == 'p'):
             self.leyendo_linea = True
+
+        elif (tag == 'span'):
+            for att in attrs:
+                if (att[0] == 'class' and att[1] == "answernumber"):
+                    self.prefijo_detectado = True
 
     def handle_endtag(self, tag):
         if (tag == 'div' and self.fase_respuesta_correcta):
             self.fase_respuesta_correcta = False
+
+            respuestas_correctas = self.respuesta_correcta_actual.split(',&')
+            for respuesta in respuestas_correctas:
+                p = self.preguntas[self.pregunta_actual-1]
+                respuesta_correcta_saneada = re.sub(r'\s+', '',
+                                                    respuesta,
+                                                    flags=re.UNICODE)
+                for index in range(len(p.respuestas)):
+                    respuesta_candidata_saneada = re.sub(r'\s+', '',
+                                                         p.respuestas[index],
+                                                         flags=re.UNICODE)
+                    print('comparando \n>{} ({}) \n>{} ({}) \n RESULTADO: {}'
+                          .format(respuesta_correcta_saneada,
+                                  len(respuesta_correcta_saneada),
+                                  respuesta_candidata_saneada,
+                                  len(respuesta_candidata_saneada),
+                                  respuesta_candidata_saneada == respuesta_correcta_saneada))
+
+                    if respuesta_correcta_saneada == respuesta_candidata_saneada:
+                        print('respuesta correcta encontrada: {}'.format(index))
+                        p.correctas.append(index)
+
         if (tag == 'div' and self.fase_enunciado):
             self.fase_enunciado = False
             self.p_count = 0
@@ -66,21 +96,20 @@ class Collector(HTMLParser):
             self.span_count += 1
 
     def handle_data(self, data):
+        # Evitamos leer los prefijos de cada respuesta (a,b,c...1,2,3... )
+        if (self.prefijo_detectado):
+            self.prefijo_detectado = False
+            print('prefijo {} detectado'.format(data))
+            return
         data = data.replace("\n", " ")
-#        print("datooos")
-#        print(data)
-#        print(self.span_count)
-#        print("leyendo_linea: {}".format(self.leyendo_linea))
-#        print("fase_enunciado: {}".format(self.fase_enunciado))
-#        print("fase_respuesta: {}".format(self.fase_respuesta))
-#        print("fase_respuesta_correcta: {}".format(self.fase_respuesta_correcta))
-#        print("p_count: {}".format(self.p_count))
         # Lectura de preguntas
         if (self.leyendo_linea and self.fase_enunciado):
             if (len(self.preguntas) < self.pregunta_actual):
                 print("guardando pregunta {0}...".format(self.pregunta_actual))
                 self.preguntas.append(Pregunta())
             p = self.preguntas[self.pregunta_actual-1]
+
+            # Unimos en caso de ser pregunta multilinea
             if (self.span_count > 0 ):
                 p.enunciado[self.p_count-1] = "".join([p.enunciado[self.p_count-1], data])
             else:
@@ -99,17 +128,29 @@ class Collector(HTMLParser):
                   .format(self.respuesta_actual, self.pregunta_actual, data))
 
         elif (self.leyendo_linea and self.fase_respuesta_correcta):
+            data = re.sub(r',\s*$', ',&', data, flags=re.UNICODE)
             print("tratando correcta {}".format(data))
             self.respuesta_correcta_actual = self.respuesta_correcta_actual + data
             p = self.preguntas[self.pregunta_actual-1]
-            for index in range(len(p.respuestas)):
-                print("Esta {0} en {1}\n? {2}".format(data, p.respuestas[index], data in p.respuestas[index]))
-                print("Filter difference {}".format(len(p.respuestas[index]) - len(data)))
-                if (abs(len(p.respuestas[index]) - len(data)) < 10  # Filtro de precisión
-                        and data in p.respuestas[index]):
-                    p.correctas.append(index)
-                    print("respuesta_correcta: {0}".format(index))
 
+
+def imprime_resultados(parser):
+    preguntas_sin_respuesta = 0
+    print("\nRESULTADOS DEL ESCANEO\n########\n#######")
+    for pregunta in parser.preguntas:
+        for enunciado in pregunta.enunciado:
+            print(enunciado, flush=True)
+        for index in range(len(pregunta.respuestas)):
+            if (index in pregunta.correctas):
+                print("+ {}".format(pregunta.respuestas[index]), flush=True)
+            else:
+                print("- {}".format(pregunta.respuestas[index]), flush=True)
+
+        if (len(pregunta.correctas) < 1):
+            preguntas_sin_respuesta += 1
+
+    if preguntas_sin_respuesta:
+        print("#####\n#Hay {} preguntas sin respuesta\n#####".format(preguntas_sin_respuesta))
 
 
 if (__name__ == "__main__"):
@@ -125,20 +166,6 @@ if (__name__ == "__main__"):
     print(len(parser.preguntas[1].enunciado))
     print("Son iguales: {0}".format(parser.preguntas[0].enunciado == parser.preguntas[1].enunciado))
 
-    print("\nRESULTADOS DEL ESCANEO\n########\n#######")
-    for pregunta in parser.preguntas:
-        for enunciado in pregunta.enunciado:
-            print(enunciado, flush=True)
-        for index in range(len(pregunta.respuestas)):
-            if (index in pregunta.correctas):
-                print("+ {}".format(pregunta.respuestas[index]), flush=True)
-            else:
-                print("- {}".format(pregunta.respuestas[index]), flush=True)
-
+    imprime_resultados(parser)
 
     print(parser.preguntas[1].enunciado)
-
-
-def parsear_html():
-    print("Iniciando análisis de html...")
-
