2c33eb96c3da3eaa3a4af338f9ef3f8007aa71bf
Raulillo <36534958+r4ulill0@users.noreply.github.com>
2022-09-10 19:30:21 +0200

    Añadido menú principal

--- src/examinator.py
+++ src/examinator.py
@@ -1,11 +1,66 @@
 import re
+import curses
+import time
 from curses import wrapper
 from argparse import ArgumentParser
 import collector
 
+KEY_UP = ('KEY_UP', 'K')
+KEY_DOWN = ('KEY_DOWN', 'J')
+KEY_SELECT = (' ', '\n')
+
+MAIN_MENU_OPCIONES = ('Examen normal',
+                      'Probar coleccion en orden',
+                      'Examen personalizado',
+                      'Salir')
+MMO_IDENTACION_BASE = 5  # Posición horizontal de las opciones del menu
+MMO_ALTURA_BASE = 30  # Altura a la que se empiezan a mostrar las opciones del menu
+MMO_SEPARACION = 2    # Separacion entre las opciones del menu principal
 
 def main(pantalla, preguntas):
-    pass
+    pantalla.nodelay(True)
+    opcion = 0
+
+    # Menu principal
+    while True:
+        pantalla.clear()
+        try:
+            seleccion = pantalla.getkey()
+        except:
+            seleccion = ''
+
+        if seleccion.upper() in KEY_UP:
+            opcion = opcion - 1 if opcion > 0 else 0
+        elif seleccion.upper() in KEY_DOWN:
+            opcion = opcion + 1 if opcion < len(MAIN_MENU_OPCIONES)-1 else len(MAIN_MENU_OPCIONES)-1
+        elif seleccion in KEY_SELECT:
+            # Gestión de selección
+            if opcion == 0:
+                pass
+            if opcion == 1:
+                pass
+            if opcion == 2:
+                pass
+            if opcion == 3:
+                break
+
+        render_main_menu(pantalla, opcion)
+
+        pantalla.refresh()
+        time.sleep(0.1)
+
+def render_main_menu(pantalla, opcion):
+    indicacion = 'Seleccione una opción:'
+
+    altura_opcion = MMO_ALTURA_BASE
+    identacion_opcion = MMO_IDENTACION_BASE
+    pantalla.addstr(altura_opcion, identacion_opcion, indicacion)
+    for menu_opt in range(len(MAIN_MENU_OPCIONES)):
+        altura_opcion += MMO_SEPARACION
+        opt = f'> {menu_opt+1} {MAIN_MENU_OPCIONES[menu_opt]}'
+        opt = opt if menu_opt == opcion else opt[1:]
+        pantalla.addstr(altura_opcion, MMO_IDENTACION_BASE, opt)
+
 
 
 def leer_preguntas(archivo, debug=False):
